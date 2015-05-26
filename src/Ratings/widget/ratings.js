5654515364e07a612ecbfe8e5cc95f98d38f0732
Gerhard Richard Edens <richard.edens@mendix.com>
2015-05-26 15:01:00 +0200

    Created new GitHub repository for the Ratings widget.

---
+++ src/Ratings/widget/ratings.js
@@ -0,0 +1,235 @@
+/*jslint white:true, nomen: true, plusplus: true */
+/*global mx, define, require, browser, devel, console, document, jQuery, ga, window, mxui, location */
+/*mendix */
+/*
+    GoogleAnalytics
+    ========================
+
+    @file      : GoogleAnalytics.js
+    @version   : 2.0.0
+    @author    : Gerhard Richard Edens
+    @date      : Wed, 20 May 2015 12:17:18 GMT
+    @copyright : Mendix b.v.
+    @license   : Apache 2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare", "mxui/widget/_WidgetBase", "dijit/_TemplatedMixin",
+    "mxui/dom", "dojo/dom", "dojo/query", "dojo/dom-prop", "dojo/dom-geometry", "dojo/dom-attr", "dojo/dom-class", "dojo/dom-style", "dojo/dom-construct", "dojo/on", "dojo/_base/lang", "dojo/text", "dojo/number", "dojo/_base/array",
+    "dojo/text!Ratings/widget/templates/ratings.html"
+], function (declare, _WidgetBase, _TemplatedMixin, 
+              domMx, dom, domQuery, domProp, domGeom, domAttr, domClass, domStyle, domConstruct, on, lang, text, number, dojoArray, widgetTemplate) {
+    "use strict";
+
+    return declare("Ratings.widget.ratings", [ _WidgetBase, _TemplatedMixin ], {
+
+        templateString    : widgetTemplate,
+
+        divNode : "",
+        attrValues : null,
+        hasVoted : true,
+        connectArray : null,
+        mouseoverArray : null,
+        root : "",
+        ratingsListEvent : "",
+        pathAttr : "",
+        pathName : "",
+
+        postCreate : function(){
+            console.log(this.id + ".postCreate");
+            this.root = location.protocol + "//" + location.host;
+
+            this.attrValues = [];
+            this.connectArray = [];
+            this.mouseoverArray = [];
+            this.pathAttr = this.voteAttr.split("/");
+            this.pathName = this.voteName.split("/");
+
+            this.initContext();
+
+            this.actRendered();
+        },
+
+        showRatings : function(mxApp){
+            console.log(this.id + ".showRatings");
+
+            /* CLEAR ALL NODES */
+            domConstruct.empty(this.divNode);
+
+            var showTotal, showCount, showVote, ratingsList, i, imgNode, ratingsLi;
+            showTotal = 0;
+            showCount = 0;
+            showVote = 0;
+
+            if (mxApp instanceof Array) {
+                mxApp = mxApp[0];
+            }
+
+            ratingsList = "";
+
+            //retrieve and calculate the vote values
+            showTotal = mxApp.getAttribute(this.ratingsTotal);
+            showCount = mxApp.getAttribute(this.ratingsCount);
+            if (showCount === 0) {
+                showVote = 1;
+            } else {
+                showVote = number.round((showTotal / showCount));
+            }
+            ratingsList = mxui.dom.create("ul");
+            if (this.voteEnabled === true) {
+                this.ratingsListEvent = this.connect(ratingsList, "onmouseleave", lang.hitch(this, this.mouseleaveEvent, showVote));
+            }
+
+            for (i = 1; i <= 5; i++) {
+                imgNode = mxui.dom.create("img",{"class": "ratings_image"});
+                if (i > showVote) {
+                    domAttr.set(imgNode, "src", (this.root + "/" + this.standardImage));
+                } else {
+                    domAttr.set(imgNode, "src", (this.root + "/" + this.mouseoverImage));
+                }
+                ratingsLi = mxui.dom.create("li", imgNode);
+                if (this.voteEnabled === true) {
+                    this.mouseoverArray[i-1] = {};
+                    this.mouseoverArray[i-1].handle = this.connect(imgNode, "onmouseenter", lang.hitch(this, this.mouseenterEvent, i));
+                    this.mouseoverArray[i-1].element = imgNode;
+                    //dojo.connect(imgNode, "onmouseleave", lang.hitch(this, this.mouseleaveEvent, i));
+                    this.connectArray[i-1] = this.connect(ratingsLi, "onclick", lang.hitch(this, this.onclickRating, i, mxApp));
+                }
+                ratingsList.appendChild(ratingsLi);
+            }
+            this.divNode.appendChild(ratingsList);
+        },
+
+        setMouseOver : function (iterator) {
+            console.log(this.id + ".setMouseOver");
+
+            var j,k;
+
+            for (j = 0; j <= iterator; j++) {
+                this.mouseoverArray[j].element.src = this.root + "/" + this.mouseoverImage;
+            }
+
+            for (k = 4; k > iterator; k--) {
+                this.mouseoverArray[k].element.src = this.root + "/" + this.standardImage;
+            }
+
+        },
+
+        onclickRating : function(count, mxApp, event) {
+            console.log(this.id + ".onclickRating");
+
+            var i, currentUserName, xpathString;
+
+            // user can click only once
+            this.disconnect(this.ratingsListEvent);
+            for (i = 0; i < this.mouseoverArray.length; i++) {
+                this.disconnect(this.mouseoverArray[i].handle);
+            }
+
+            this.setMouseOver(count-1);
+
+            // store the fact that the user has voted
+            currentUserName = mx.session.getUserName();
+            xpathString = "//" + this.pathName[1] + "[" + this.pathName[2] + " = '" + currentUserName + "']"+"["+ this.pathName[0] + " = '" + mxApp.getGUID() +  "']";
+
+            mx.data.get({
+                xpath    : xpathString,
+                callback : lang.hitch(this, this.commitRating, count, mxApp)
+            });
+        },
+
+        commitRating : function (count, mxApp, mxVote) {
+            console.log(this.id + ".commitRating");
+
+            var currentTotal, currentCount, userVote, voteDiff;
+
+            currentTotal = mxApp.getAttribute(this.ratingsTotal);
+            currentCount = mxApp.getAttribute(this.ratingsCount);
+
+            if (mxVote.length === 0) { //user has not voted before
+                this.createVote(mx.session.getUserName(), count, mxApp, currentTotal, currentCount);
+            } else { //user has voted before, update the vote
+                userVote = mxVote[0].getAttribute(this.pathAttr[2]);
+                voteDiff = count - userVote;
+
+                mxVote[0].setAttribute(this.pathAttr[2], count);
+                mxVote[0].saveSequence();
+
+                mxApp.setAttribute(this.ratingsTotal, (parseInt(currentTotal, 10) + voteDiff));
+                mxApp.saveSequence();
+            }
+        },
+
+        createVote : function (user, vote, app, currentTotal, currentCount) {
+            console.log(this.id + ".createVote");
+
+            mx.processor.createObject({
+                "className"	: this.pathName[1],
+                "callback"	: lang.hitch(this,
+
+                                         function (user, vote, app, currentTotal, currentCount, voteObject) {
+                    console.log(this.id + ".setVote");
+
+                    app.addReference(this.pathName[0], voteObject.getGUID());
+                    voteObject.setAttribute(this.pathName[2], user);
+                    voteObject.setAttribute(this.pathAttr[2], vote);
+
+                    app.setAttribute(this.ratingsTotal, (parseInt(currentTotal, 10) + vote));
+                    app.setAttribute(this.ratingsCount, (parseInt(currentCount, 10) + 1));
+
+                    app.saveSequence();
+                    voteObject.saveSequence();
+                },
+                                         user,
+                                         vote,
+                                         app,
+                                         currentTotal,
+                                         currentCount
+                                        ),
+                "context"	: null
+            });
+        },
+
+        mouseenterEvent : function(enterIterator, event) {
+            console.log(this.id + ".mouseenterEvent");
+
+            this.setMouseOver(enterIterator-1);
+        },
+
+        mouseleaveEvent : function(showVote, event) {
+            console.log(this.id + ".mouseleaveEvent");
+
+            this.setMouseOver(showVote-1);
+        },
+
+        applyContext : function(context, callback){
+            console.log(this.id + ".applyContext");
+            if (context) {
+                mx.data.get({
+                    guid: context.getActiveGUID(), 
+                    callback: lang.hitch(this, this.showRatings)
+                });
+            } else {
+                console.warm(this.id + ".applyContext received empty context");
+            }
+            callback();
+        },
+
+        uninitialize : function(){
+            var i;
+            console.log(this.id + ".uninitialize");
+            dojoArray.forEach(this.connectArray, this.disconnect);
+            for (i = 0; i < this.mouseoverArray.length; i++) {
+                this.disconnect(this.mouseoverArray[i].handle);
+            }
+        }
+    });
+});
+require(["Ratings/widget/ratings"], function () {
+    "use strict";
+});
---
+++ src/Ratings/widget/ratings_nologic.js
@@ -0,0 +1,170 @@
+/*jslint white:true, nomen: true, plusplus: true */
+/*global mx, define, require, browser, devel, console, document, jQuery, ga, window, mxui, location */
+/*mendix */
+/*
+    GoogleAnalytics
+    ========================
+
+    @file      : GoogleAnalytics.js
+    @version   : 2.0.0
+    @author    : Gerhard Richard Edens
+    @date      : Wed, 20 May 2015 12:17:18 GMT
+    @copyright : Mendix b.v.
+    @license   : Apache 2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare", "mxui/widget/_WidgetBase",
+    "mxui/dom", "dojo/dom", "dojo/query", "dojo/dom-prop", "dojo/dom-geometry", "dojo/dom-attr", "dojo/dom-class", "dojo/dom-style", "dojo/dom-construct", "dojo/on", "dojo/_base/lang", "dojo/text", "dojo/number",
+    "dojo/text!Ratings/widget/templates/ratings.html"
+], function (declare, _WidgetBase, 
+              domMx, dom, domQuery, domProp, domGeom, domAttr, domClass, domStyle, domConstruct, on, lang, text, number, dojoArray, widgetTemplate) {
+    "use strict";
+
+    return declare("Ratings.widget.ratings_nologic", [_WidgetBase], {
+        
+        name : "",
+        voteEnabled : false,
+        standardImage : "",
+        mouseoverImage : "",
+
+        divNode : "",
+        mouseoverArray : null,
+        root : location.protocol + "//" + location.host,
+        ratingsListEvent : "",	
+
+        oldvalue : 0,
+        newvalue : 0,
+
+        //summery : stub function, will be used or replaced by the client environment
+        onChange : function(){
+        },
+
+        //returns the value of this widget
+        _getValueAttr : function(){
+            return this.newvalue;
+        },
+
+        //provides th value of this widget
+        _setValueAttr : function(value){
+            this.oldvalue = parseInt(value, 10);
+            this.newvalue = parseInt(value, 10);
+            if (this.newvalue > 0) {
+                this.setRating(this.newvalue);
+            } else {
+                if (this.voteEnabled === true) {//set default value
+                    this.newvalue = 1; //do not detach
+                    this.onChange();
+                }
+                this.showCurrentValue();
+            }
+        },
+
+        _setDisabledAttr : function(value) {
+            //donothing, depends on widget configuration
+            //this.voteEnabled = !!value; 
+        },
+
+        postCreate : function(){
+            console.debug(this.id + ".postCreate");
+
+            var ratingsList, i, imgNode, ratingsLi;
+            
+            this.mouseoverArray = [];
+
+            domClass.add(this.domNode, "ratings_widget");
+            ratingsList = mxui.dom.create("ul");
+
+            if (this.voteEnabled === true) {
+                this.ratingsListEvent = this.connect(ratingsList, "onmouseleave", this.hitch(this, this.showCurrentValue));
+            }
+            for (i = 1; i <= 5; i++) {
+                imgNode = mxui.dom.create("img",{"class": "ratings_image"});
+                ratingsLi = mxui.dom.create("li", imgNode);
+
+                this.mouseoverArray[i-1] = {};
+                this.mouseoverArray[i-1].element = imgNode;
+                if (this.voteEnabled === true) { //can vote and not voted before
+                    this.mouseoverArray[i-1].handlein = this.connect(imgNode, "onmouseenter", this.hitch(this, this.displayImages, i));
+                    this.mouseoverArray[i-1].handleout = this.connect(imgNode, "onmouseleave", this.hitch(this, this.displayImages, i-1));
+                    this.mouseoverArray[i-1].handleclick = this.connect(ratingsLi, "onclick", this.hitch(this, this.setRating, i));
+                }
+                ratingsList.appendChild(ratingsLi);
+            }
+
+            this.domNode.appendChild(ratingsList);
+            this.showCurrentValue();
+            //NOT for attribute widgets: this.actRendered();
+        },
+
+        showCurrentValue : function () {
+            this.displayImages(this.newvalue);
+        },
+
+        displayImages : function (iterator) {
+            console.debug(this.id + ".displayImages");
+            var j, k;
+            if (iterator < 0 || iterator > 5) {
+                return;
+            }
+
+            for (j = 0; j <= iterator-1; j++) {
+                domAttr.set(this.mouseoverArray[j].element, "src", this.root + "/" + this.mouseoverImage);
+            }
+
+            for (k = 4; k > iterator-1; k--) {
+                domAttr.set(this.mouseoverArray[k].element, "src", this.root + "/" + this.standardImage);
+            }
+        },
+
+        setRating : function(count) {
+            var i;
+            console.debug(this.id + ".onclickRating");
+            this.newvalue = count;
+            if (this.newvalue !== this.oldvalue) {
+                this.onChange();
+            }
+
+            this.displayImages(count);
+            for (i = 0; i < this.mouseoverArray.length; i++) {
+                if (this.mouseoverArray[i].handlein){
+                    this.disconnect(this.mouseoverArray[i].handlein);
+                }
+                if (this.mouseoverArray[i].handleout) {
+                    this.disconnect(this.mouseoverArray[i].handleout);
+                }
+            }
+        },
+
+
+        mouseleaveEvent : function(showVote, event) {
+            console.debug(this.id + ".mouseleaveEvent");
+            this.setMouseOver(showVote-1);
+        },
+
+        uninitialize : function(){
+            var i;
+            console.debug(this.id + ".uninitialize");
+            for (i = 0; i < this.mouseoverArray.length; i++){
+                if (this.mouseoverArray[i].handlein){
+                    this.disconnect(this.mouseoverArray[i].handlein);
+                }
+                if (this.mouseoverArray[i].handleout) {
+                    this.disconnect(this.mouseoverArray[i].handleout);
+                }
+                if (this.mouseoverArray[i].handleclick) {
+                    this.disconnect(this.mouseoverArray[i].handleclick);
+                }
+            }
+        }
+    });
+    
+});
+require(["Ratings/widget/ratings_nologic"], function () {
+    "use strict";
+});
